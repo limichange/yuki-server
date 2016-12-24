fceaf8b882e8ea6af183592f5492b035c71f2a4e
limichange <limichange@hotmail.com>
2016-12-24 10:15:32 +0800

    add signIn api

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ server.use(cors())
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(restc.express());
+
+const userJSON = {
+  nickname: 'houyao',
+  uuid: '23ij293j922un392838hklbkbkbs82',
+  avatar: 'http://tva3.sinaimg.cn/crop.0.0.180.180.50/6ecbbfd0jw1e8qgp5bmzyj2050050aa8.jpg'
+}
+
 server.get('/me', (req, res) => {
-  return res.json({
-    nickname: 'houyao',
-    uuid: '23ij293j922un392838hklbkbkbs82',
-    avatar: 'http://tva3.sinaimg.cn/crop.0.0.180.180.50/6ecbbfd0jw1e8qgp5bmzyj2050050aa8.jpg'
-  })
+  return res.json(userJSON)
+})
+
+server.post('/user/signIn', (req, res) => {
+  console.log(req.body)
+  return res.json(userJSON)
 })
 
 server.listen(PORT, () => {
